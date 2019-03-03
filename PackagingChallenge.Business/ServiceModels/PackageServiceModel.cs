f68f1554607f1d93fd301046fd6855d5ca303718
Dusan <dusan_jel@hotmail.com>
2019-03-03 17:20:58 +0100

    Rename files.

--- PackagingChallenge.Business/ServiceModels/PackageServiceModel.cs
+++ PackagingChallenge.Business/ServiceModels/PackageServiceModel.cs
@@ -1,18 +1,14 @@
-﻿using PackagingChallenge.Utils.Support;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using static PackagingChallenge.Utils.Support.Enums;
 
 namespace PackagingChallenge.Business.ServiceModels
 {
-    public class PackageListServiceModel
+    public class PackageServiceModel
     {
         //TODO describe and document fields for swagger
-        // Use http://localhost:port/swagger/ to access swagger test page.
-        public int IndexNumber { get; set; }
-        public decimal Weight { get; set; }
-        public decimal Cost { get; set; }
-        public Items Item { get; set; }
+        /// Use http://localhost:port/swagger/ to access swagger test page.
+        public int MaxWeight { get; set; }
+        public List<PackageListServiceModel> PackageList { get; set; }
     }
 }
